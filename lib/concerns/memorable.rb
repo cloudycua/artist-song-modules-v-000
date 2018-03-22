dadf1f3c9d5ea5f45c48907758ad70752ee04dd7
Christine Cua <cloudycua@gmail.com>
2018-03-22 19:27:33 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,12 @@
 module Memorable
 
   module ClassMethods
-    def self.reset_all
+    def reset_all
       @@songs.clear
      end
 
-    def self.count
-      self.all.count
+    def count
+      all.count
     end
   end
 
